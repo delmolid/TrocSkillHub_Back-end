fc0e971d5a93066cb6564accf1d7658240366879
Molid <delmolid@hotmail.fr>
2026-02-20 09:29:30 +0100

    add authentitation feature to login with users databases

--- TrocSkillHub/src/main/java/RNCP/TrocSkillHub/TrocSkillHubApplication.java
+++ TrocSkillHub/src/main/java/RNCP/TrocSkillHub/TrocSkillHubApplication.java
@@ -2,8 +2,7 @@ package RNCP.TrocSkillHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
+
 
 @SpringBootApplication
 public class TrocSkillHubApplication {
---
+++ TrocSkillHub/src/main/java/java/java/Long.java
@@ -0,0 +1,5 @@
+package java.java;
+
+public class Long {
+
+}
